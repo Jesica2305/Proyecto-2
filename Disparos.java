4a7501012d8d1f6ac0ea87c8bb68482153ae826d
Jesica2305 <142047118+Jesica2305@users.noreply.github.com>
2023-10-12 21:19:36 -0500

    Disparos.java

--- Disparos.java
+++ Disparos.java
@@ -1,10 +1,18 @@
 public class Disparos {
     private Tablero tablero;
 
+    /**
+      * Constructor de la clase Disparos el cual recibe un objeto Tablero y se asigna con la variable tablero para que Disparos interactue con ese tablero.
+      * Complejidad Temporal: O(1) Tiempo constante
+      */
     public Disparos(Tablero tablero) {
         this.tablero = tablero;
     }
 
+    /**
+      * Método para verificar el disparo del usuario en el tablero
+      * Complejidad Temporal: O(1) Tiempo constante
+      */
     public boolean verificarCoordenada(int filaUsuario, int columnaUsuario) {
         if (filaUsuario >= 0 && filaUsuario < tablero.getTAMANO() && columnaUsuario >= 0 && columnaUsuario < tablero.getTAMANO()) {
             char objeto = tablero.getMatriz()[filaUsuario][columnaUsuario];
@@ -14,7 +22,7 @@ public class Disparos {
             }
             if (objeto == '.') {
                 System.out.println("Usted ya disparo aqui!");
-                return false;
+                return false;//Devuelve false si dispara en una coordenada que ya disparo
             }
             if (objeto != '*') {
                     System.out.println("¡Hay un barco en esa coordenada! (" + objeto + ")");
@@ -31,7 +39,5 @@ public class Disparos {
         }
         return false; // Devuelve false si no se acertó un barco o la coordenada estaba fuera de rango
     }
-
-
 }
 
