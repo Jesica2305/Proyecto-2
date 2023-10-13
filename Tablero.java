ebf7493422d2e032a3355ff7f49668931047d529
Jesica2305 <142047118+Jesica2305@users.noreply.github.com>
2023-10-12 20:58:41 -0500

    Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -2,19 +2,37 @@ public class Tablero {
     private char[][] matriz;
     private int TAMANO;
 
+    /**
+      * Constructor que recibe el tamaño del tablero como parametro
+      * Inicializa la matriz con el tamaño especificado
+      * Complejidad Temporal: O(1) Tiempo constante
+      */
     public Tablero(int TAMANO) {
         this.TAMANO = TAMANO;
         matriz = new char[TAMANO][TAMANO];
     }
-
+    /**
+      * Método para obtener la matriz
+      * @return Retorna la matriz
+      * Complejidad Temporal: O(1) Tiempo constante
+      */
     public char[][] getMatriz() {
         return matriz;
     }
-
+    
+    /**
+      * Método para obtener el tamaño
+      * @return Retorna el tamaño
+      * Complejidad Temporal: O(1) Tiempo constante
+      */
     public int getTAMANO() {
         return TAMANO;
     }
 
+    /**
+      * Se inicia la matriz con asteriscos
+      * Complejidad Temporal: O(N^2) Complejidad cuadrática
+      */
     public void inicializarMatriz() {
         for (int i = 0; i < TAMANO; i++) {
             for (int j = 0; j < TAMANO; j++) {
@@ -23,6 +41,10 @@ public class Tablero {
         }
     }
 
+    /**
+      * Se imprime la matriz 
+      * Complejidad Temporal: O(N^2) Complejidad cuadrática
+      */
     public void imprimirMatriz() {
         System.out.println("Matriz con barcos:");
         for (int i = 0; i < TAMANO; i++) {
@@ -33,9 +55,12 @@ public class Tablero {
         }
     }
 
-
+    /**
+      * Se actualiza la matriz del tablero
+      * Complejidad Temporal: O(1) Tiempo constante
+      */
     public void setMatriz(char[][] matriz) {
-        this.matriz = this.matriz;
+        this.matriz = matriz;
     }
 }
 
